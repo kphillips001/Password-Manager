a1b7f8a69078e31b43db1f95c7b95aa479176b0e
Kevin <knphillips01@gmail.com>
2021-01-26 15:53:39 -0600

    added entries and buttons

--- main.py
+++ main.py
@@ -19,15 +19,29 @@ canvas.grid(column=1, row=0)
 
 # Labels
 website_label = Label(text="Website:")
-website_label.grid(column=0, row=1)
+website_label.grid(row=1, column=0 )
 
 email_label = Label(text="Email/Username:")
-email_label.grid(column=0, row=2)
+email_label.grid(row=2, column=0)
 
 password_label = Label(text="Password:")
-password_label.grid(column=0, row=3)
+password_label.grid(row=3, column=0)
 
 # Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
 
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+
+password_entry = Entry(width=31)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+
+add_button = Button(text="Add")
+add_button.grid(row=4, column=1)
 
 window.mainloop()
