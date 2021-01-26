d4499129bbff72ff3c6967b9f127c9f87371de1f
Kevin <knphillips01@gmail.com>
2021-01-26 15:34:27 -0600

    finished up labels

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 # Have to use .pack or .grid
-canvas.pack()
+# canvas.pack()
+canvas.grid(column=1, row=0)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+# Entries
+
 
 window.mainloop()
