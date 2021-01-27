cc558c8fff61e90a3bea8e48fa3c3921d0faf51a
Kevin <knphillips01@gmail.com>
2021-01-27 16:39:25 -0600

    installed pyperclip and finished project

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+
+  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+  # nr_letters = random.randint(8, 10)
+  # nr_symbols = random.randint(2, 4)
+  # nr_numbers = random.randint(2, 4)
+
+  # list comprehension to replace the for loops
+  password_letters = [choice(letters) for _ in range(randint(8, 10))]
+  password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+  password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+  # for char in range(nr_letters):
+  #   password_list.append(random.choice(letters))
+
+  # for char in range(nr_symbols):
+  #   password_list += random.choice(symbols)
+
+  # for char in range(nr_numbers):
+  #   password_list += random.choice(numbers)
+
+  password_list = password_letters + password_symbols + password_numbers
+  shuffle(password_list)
+
+  password = "".join(password_list)
+  # password = ""
+  # for char in password_list:
+  #   password += char
+
+  # print(f"Your password is: {password}")
+  password_entry.insert(0, password)
+  pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -52,7 +89,7 @@ password_entry = Entry(width=17)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", width=14)
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=30, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
